0d3acf7a6ead2fafd595edd69d44275e4f643f41
Myask-foo <kittyslillian@gmail.com>
2025-05-23 06:22:03 -0600

    fix CoModException

--- src/main/java/invalid/myask/stairdown/blocks/BlockHollowLog.java
+++ src/main/java/invalid/myask/stairdown/blocks/BlockHollowLog.java
@@ -28,7 +28,7 @@ public class BlockHollowLog extends BlockRotatedPillar {
     }
 
     @Override
-    public void addCollisionBoxesToList(World worldIn, int x, int y, int z, AxisAlignedBB mask, List<AxisAlignedBB> list2, Entity collider) {
+    public void addCollisionBoxesToList(World worldIn, int x, int y, int z, AxisAlignedBB mask, List<AxisAlignedBB> outputList, Entity collider) {
         List<AxisAlignedBB> list = new ArrayList<>();
         int meta = worldIn.getBlockMetadata(x, y, z);
         if ((meta & 12) != 0) {
@@ -44,7 +44,7 @@ public class BlockHollowLog extends BlockRotatedPillar {
             list.add(AxisAlignedBB.getBoundingBox(x,y,z+(14/16D), x+1, y+1, z+1));
         }
         for (AxisAlignedBB aabb : list)
-            if (aabb.intersectsWith(mask)) list.add(aabb);
+            if (aabb.intersectsWith(mask)) outputList.add(aabb);
     }
 
     @Override
