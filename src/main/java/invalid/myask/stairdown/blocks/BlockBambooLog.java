8d0ddefafb8d890a801b9e414087f3fc99f08ecb
Myask-foo <kittyslillian@gmail.com>
2025-05-23 07:15:45 -0600

    start bamboo "log"

---
+++ src/main/java/invalid/myask/stairdown/blocks/BlockBambooLog.java
@@ -0,0 +1,24 @@
+package invalid.myask.stairdown.blocks;
+
+import invalid.myask.stairdown.StairDownBlocks;
+import net.minecraft.block.BlockLog;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+
+public class BlockBambooLog extends BlockLog {
+    public static final String[] TEXTURE_SUB_NAMES = new String[] {"bamboo"};
+    BlockBambooLog() {
+        super();
+        setCreativeTab(StairDownBlocks.stair_down_tab);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        field_150167_a = new IIcon[TEXTURE_SUB_NAMES.length]; //side
+        field_150166_b = new IIcon[TEXTURE_SUB_NAMES.length]; //top
+        for (int i = 0; i < TEXTURE_SUB_NAMES.length; i++) {
+            this.field_150167_a[i] = reg.registerIcon(this.getTextureName() + "_" + TEXTURE_SUB_NAMES[i]);
+            this.field_150166_b[i] = reg.registerIcon(this.getTextureName() + "_" + TEXTURE_SUB_NAMES[i] + "_top");
+        }
+    }
+}
