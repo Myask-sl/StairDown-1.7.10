d19fc95e67c00a11d55e18b4a6f41d9fe416c3be
Myask-foo <kittyslillian@gmail.com>
2025-05-23 22:00:14 -0600

    remember to register render code so it does thing

--- src/main/java/invalid/myask/stairdown/ClientProxy.java
+++ src/main/java/invalid/myask/stairdown/ClientProxy.java
@@ -1,8 +1,17 @@
 package invalid.myask.stairdown;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import invalid.myask.stairdown.client.HollowLogRenderer;
+
 public class ClientProxy extends CommonProxy {
 
     // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
     // Don't forget to call the super methods as well.
 
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+        RenderingRegistry.registerBlockHandler(HollowLogRenderer.instance);
+    }
 }
