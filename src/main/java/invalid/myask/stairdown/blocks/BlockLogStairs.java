eae104a47e6dfc52cbffa3307c78c6013502b4dd
Myask-foo <kittyslillian@gmail.com>
2025-05-24 03:58:44 -0600

    stair icon parentmeta

--- src/main/java/invalid/myask/stairdown/blocks/BlockLogStairs.java
+++ src/main/java/invalid/myask/stairdown/blocks/BlockLogStairs.java
@@ -1,9 +1,15 @@
 package invalid.myask.stairdown.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.util.IIcon;
 
 public class BlockLogStairs extends BlockMadeStairs {
     public BlockLogStairs(Block parent, int meta) {
         super(parent, meta);
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta) {
+        return parentBlock.getIcon(side, parentMeta);
     }
 }
