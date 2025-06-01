33868f46287fdca85b8f56fc939e306f5caefcaa
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2025-05-31 19:14:47 -0600

    ctors?

--- src/main/java/invalid/myask/stairdown/items/ItemSlabFromParent.java
+++ src/main/java/invalid/myask/stairdown/items/ItemSlabFromParent.java
@@ -2,19 +2,18 @@ package invalid.myask.stairdown.items;
 
 import invalid.myask.stairdown.blocks.BlockMadeSlab;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockSlab;
 import net.minecraft.item.ItemSlab;
 import net.minecraft.realms.RealmsMth;
 
 public class ItemSlabFromParent extends ItemSlab {
-    BlockMadeSlab parent;
-    public ItemSlabFromParent(Block thisBlock, Block parent, BlockMadeSlab slab, BlockMadeSlab doubleSlab, Boolean isDouble) {
+    BlockMadeSlab theSlab;
+    public ItemSlabFromParent(Block thisBlock, BlockMadeSlab slab, BlockMadeSlab doubleSlab, Boolean isDouble) {
         super(thisBlock, slab, doubleSlab, isDouble);
-        parent = (BlockMadeSlab) parent;
+        theSlab = (BlockMadeSlab) thisBlock;
     }
 
     @Override
     public int getMetadata(int damage) {
-        return RealmsMth.clamp(damage, parent.getParentMeta(), parent.getMetaMax());
+        return RealmsMth.clamp(damage, theSlab.getParentMeta(), theSlab.getMetaMax());
     }
 }
